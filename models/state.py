f6f835580db53cc96d05380e3c2ceefda5ae21d4
Christian-Ashley <116812977+Christian-Ashley@users.noreply.github.com>
2024-02-02 19:57:15 +0000

    communism joke

--- models/state.py
+++ models/state.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"comrade! everything belongs to this class!"
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """I am in a State of distress"""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
