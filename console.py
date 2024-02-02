4000c0e0858b0c6ca9138d0cb17dadabb2434cb3
iamseaniam <7222@holbertonstudents.com>
2024-02-02 14:34:53 -0600

    Documentation for console.py

--- console.py
+++ console.py
@@ -5,23 +5,28 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter class"""
-    
+
     prompt = "(hbnb) "  
-    
+
     def do_quit(self, arg):
+        """Quit command to exit the program"""
         return True
-    
+
     def do_EOF(self,arg):
+        """Exit the program when EOF is reached"""
         print()
         return True
-    
+
     def emptyline(self):
+        """Do nothing on empty input line"""
         pass
-    
+
     def help_quit(self):
+        """Help message for the quit command"""
         print("Quit command to exit the PRogram")
-        
+
     def help_EOF(self):
+        """Help message for the EOF command"""
         print("Exit mthe program when End-of_File (EOF) is reached")
 
 if __name__ == '__main__':
