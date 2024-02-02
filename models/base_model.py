2df4e46f04c7d025b8a10d9d65e51ad4043b1b24
Sean M <143843703+iamseaniam@users.noreply.github.com>
2024-02-02 13:19:57 -0600

    Sean's personal branch (#1)
    * All test files i will be using
    
    * Test file for base model. correct attributes. update and to_dict

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""BaseModel"""
+
+import uuid
+import datetime
+
+class BaseModel():
+    """better documentation coming soon"""
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """Checks for id. If it doesn't have id, a new one is created along with the current time."""
+        if id is not None:
+            self.id = id
+        else:
+            new_uuid = uuid.uuid4()
+            self.id = str(new_uuid)
+            
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+        
+    def update(self):
+        self.updated_at = datetime.datetime.now()
